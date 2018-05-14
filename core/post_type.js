4469d61ffe6c6c156218ad8f160a806c31be5bc5
Gabriel Cerqueira de Castro <gcastro@into.net>
2018-05-14 18:19:04 -0300

    “All”

---
+++ core/post_type.js
@@ -0,0 +1,14 @@
+﻿var registered_post_type = ["Default"];
+
+function PostType() {
+	this.register_post_type = function (name, args) {
+		registered_post_type.push(name);
+	}
+
+	this.getPostTypes = function () {
+		return registered_post_type;
+	}
+}
+
+
+module.exports = PostType;
